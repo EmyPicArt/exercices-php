2d0459ccb2311283ed7a5869b635fd41c7144370
EmyPicArt <93980104+EmyPicArt@users.noreply.github.com>
2021-11-25 11:52:22 +0100

    fin ex 2

--- ex 2/index.php
+++ ex 2/index.php
@@ -1,4 +1,7 @@
 <?php
-// déclarer une variable
-    $test = 'test';
+// déclarer une variable 
+    $lastname = 'PICART';
+    $firstname = 'Noémie';
+    $age = 33;
+    echo  $lastname, '<br />', $firstname, '<br />', $age, ' ans' ;
 ?>
