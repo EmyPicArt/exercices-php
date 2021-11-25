461652f805b8696d776aad6b9510cf308c3fcb19
EmyPicArt <93980104+EmyPicArt@users.noreply.github.com>
2021-11-25 15:16:16 +0100

    fin ex 1 if else majeur/ mineur

---
+++ ex 1 - partie 2/index.php
@@ -0,0 +1,9 @@
+<?php
+// Ex 1
+   $age = 16;
+   if($age >= 18){
+      echo 'Vous êtes majeur';
+   }else{
+      echo 'Vous êtes mineur';
+   }
+ 
---
+++ ex 3 - partie 2/index.php
@@ -0,0 +1,15 @@
+<?php
+// Ex 1 ___déclarer 2 variables : object et message
+   $object = 'Objet du message : ';
+   $message = 'Texte du message';
+   // echo pour afficher ses variable
+   echo $object, $message;
+   ?>
+   
+   <!-- Pour écrire dans un paragraphe html -->
+   <!-- <p>< ?= $object ?></p>
+   <p>< ?=$message ?> ?></p> -->
+
+/* < ?= $object, $message; ?>
+< ?= ?> == echo */
+
