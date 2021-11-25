8829be2d4a653a6b0c9447336e0d4098c14f7a7e
EmyPicArt <93980104+EmyPicArt@users.noreply.github.com>
2021-11-25 14:09:37 +0100

    correction ex 1

--- ex 1/index.php
+++ ex 1/index.php
@@ -1,5 +1,15 @@
 <?php
 // Ex 1 ___déclarer 2 variables : object et message
-   echo $object = 'Objet du message : ';
-   echo $message = 'Texte du message';
-?>
+   $object = 'Objet du message : ';
+   $message = 'Texte du message';
+   // echo pour afficher ses variable
+   echo $object, $message;
+   ?>
+   
+   <!-- Pour écrire dans un paragraphe html -->
+   <!-- <p>< ?= $object ?></p>
+   <p>< ?=$message ?> ?></p> -->
+
+/* < ?= $object, $message; ?>
+< ?= ?> == echo */
+
