87018e4badf0688015fab872f1e5df968961201b
EmyPicArt <93980104+EmyPicArt@users.noreply.github.com>
2021-11-25 14:28:30 +0100

    correction ex 4

--- ex 4/index.php
+++ ex 4/index.php
@@ -2,3 +2,5 @@
     $name = 'Noémie';
     echo 'Bonjour ' .$name. ' , comment vas-tu ?';
 ?>
+<!-- Si on met du html il ne faut pas fermer la balise php -->
+<p>Bonjour <?! $name ?> comment vas-tu ?</p>
