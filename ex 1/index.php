872ed693f2c77b0c020a24af203be4df11a303f0
EmyPicArt <93980104+EmyPicArt@users.noreply.github.com>
2021-11-25 10:21:36 +0100

    fin ex 1

--- ex 1/index.php
+++ ex 1/index.php
@@ -1,4 +1,5 @@
 <?php
-// déclarer une variable
-    $test = 'test';
+// Ex 1 ___déclarer 2 variables : object et message
+   echo $object = 'Objet du message : ';
+   echo $message = 'Texte du message';
 ?>
