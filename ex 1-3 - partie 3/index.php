b63ec715b6e7c967e543a66087878b1d638a7132
EmyPicArt <93980104+EmyPicArt@users.noreply.github.com>
2021-11-26 09:39:35 +0100

    fin ex 2 part 3 : afficher la valeur d'un index []

--- ex 1-3 - partie 3/index.php
+++ ex 1-3 - partie 3/index.php
@@ -1,6 +1,7 @@
 <?php
     $months=['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'];
     var_dump($months) ;
+    echo $months[5];
 
 ?>
 <!-- $months= array('Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'); -->
