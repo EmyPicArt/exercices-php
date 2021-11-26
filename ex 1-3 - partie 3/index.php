04041ace9313c60c3ff91f53bc45ea1737b646c1
EmyPicArt <93980104+EmyPicArt@users.noreply.github.com>
2021-11-26 11:50:34 +0100

    correctif part 2 (2) et par 3 (1-3)

--- ex 1-3 - partie 3/index.php
+++ ex 1-3 - partie 3/index.php
@@ -6,4 +6,17 @@
     }
 
 ?>
+<!-- Méthode pour le tableau array -->
 <!-- $months= array('Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'); -->
+
+<!-- Méthode 2 -->
+<!-- for ($i=0; $i<count($months);$i++){
+    echo $months[$i];
+} -->
+
+<!-- Méthode 3 -->
+<!-- $i=0;
+while($i<count($months)){
+    echo $months[$i];
+    $i++;
+} -->
--- ex 2 - partie 2/index.php
+++ ex 2 - partie 2/index.php
@@ -4,13 +4,13 @@
    $gender = 'Femme';
    if($gender == 'Homme' && $age>=18){
       echo 'Vous êtes un homme et vous êtes majeur';
-   }elseif($gender == 'Homme' && $age<=18){
+   }elseif($gender == 'Homme' && $age<18){
       echo 'Vous êtes un homme et vous êtes mineur';
    }
    elseif($gender == 'Femme' && $age>=18){
       echo 'Vous êtes une femme et vous êtes majeure';
    }
-   elseif($gender == 'Femme' && $age<=18){
+   else{
       echo 'Vous êtes une femme et vous êtes mineure';
    }
    ?>
