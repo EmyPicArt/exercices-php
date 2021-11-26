a9c428f9987693a648ca0bed35b57100027d47e1
EmyPicArt <93980104+EmyPicArt@users.noreply.github.com>
2021-11-26 10:07:16 +0100

    fin ex 4 part 3 tableau associatif

--- ex 4-6 - partie 3/index.php
+++ ex 4-6 - partie 3/index.php
@@ -1,5 +1,11 @@
 <?php
-    $number = (140 + 30)/2;
-    echo $number;
+    $departements = [
+        '02' => 'Aisne',
+        '59' => 'Nord',
+        '60' => 'Oise',
+        '62' => 'Pas-de-Calais',
+        '80' => 'Somme',
+    ];
+    var_dump($departements);
 
 ?>
