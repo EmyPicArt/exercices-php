664151c25c21b5c38a269589bc181336e7fd83e1
EmyPicArt <93980104+EmyPicArt@users.noreply.github.com>
2021-11-29 10:36:19 +0100

    fin ex 3 part 4 : function et if/else

--- ex 3  - partie 4/index.php
+++ ex 3  - partie 4/index.php
@@ -1,5 +1,28 @@
 <?php
-    $number = (140 + 30)/2;
-    echo $number;
+    $entier1 = 10;
+    $entier2 = 10;
 
+// Je nomme ma fonction "compare" et je lui attribue 2 paramètres ("$paramEntier1 et $paramEntier2")
+    function compare ($paramEntier1, $paramEntier2){
+        // Si $entier1 est plus grand que $entier2 alors je retourne '+grand'
+        if($paramEntier1 > $paramEntier2) {
+            return 'Le premier nombre est le plus grand';
+        }elseif ($paramEntier1 < $paramEntier2){
+            return 'Le premier nombre est le plus petit';
+        }else {
+            return 'Les deux nombres sont identiques';
+        };
+    }
+    // je crée une variable résultat et je récupére ma fonction et mes nombres
+    $result =  compare ($entier1, $entier2);
+    // j'affiche le résultat
+    echo $result;
+
+
+
+// function calcul($paramEntier1, $paramEntier2){
+//     return ($paramEntier1 + $paramEntier2);
+// }
+// $result = calcul ($entier1, $entier2);
+// echo $result;
 ?>
